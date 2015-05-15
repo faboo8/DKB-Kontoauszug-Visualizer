5860fabef7737b43ef1ae62480622b7560b499a3
balzer82 <balzer82@gmail.com>
2015-05-15 13:12:25 +0200

    Code fix

--- DKB-Kontoauszug-Visualizer.py
+++ DKB-Kontoauszug-Visualizer.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[218]:
+# In[27]:
 
 import pandas as pd
 get_ipython().magic(u'matplotlib inline')
@@ -9,20 +9,20 @@ get_ipython().magic(u'matplotlib inline')
 
 # ![DKB Logo](http://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Deutsche-Kreditbank-AG-Logo.svg/500px-Deutsche-Kreditbank-AG-Logo.svg.png)
 
-# In[238]:
+# In[28]:
 
 knr = '1808150815' # type in your KNr here!
 kst = 1000.0 # EUR zu Beginn
 
 
-# In[220]:
+# In[29]:
 
 data = pd.read_csv(knr+'.csv', index_col='Buchungstag',
                    header=4, sep=';', quoting=1, parse_dates=True,
                    encoding='latin-1', dayfirst=True)
 
 
-# In[221]:
+# In[30]:
 
 #data.head(5)
 
@@ -34,24 +34,24 @@ data = pd.read_csv(knr+'.csv', index_col='Buchungstag',
 
 # ### Fix European Thousands and Comma String Problem
 
-# In[222]:
+# In[31]:
 
 data['Betrag (EUR)'] = data['Betrag (EUR)'].str.replace('.','')
 data['Betrag (EUR)'] = data['Betrag (EUR)'].str.replace(',','.')
 data['Betrag (EUR)'] = data['Betrag (EUR)'].astype('float')
 
 
-# In[223]:
+# In[32]:
 
 # Anon the data for public
 #import random
-#data['Betrag (EUR)'] = [50-random.random()*100 for rv in range(len(data['Betrag (EUR)']))]
+#data['Betrag (EUR)'] = [-50+random.random()*200 for rv in range(len(data['Betrag (EUR)']))]
 #data[u'Auftraggeber / Begünstigter'] = ['Empf/Sender '+str(i) for i in range(len(data['Betrag (EUR)']))]
 
 
 # ### Rename some things
 
-# In[224]:
+# In[33]:
 
 data[u'Auftraggeber / Begünstigter'].replace('DKB AG', 'DKB Broker', inplace=True)
 data[u'Auftraggeber / Begünstigter'].replace('DKB VISACARD', 'Bar', inplace=True)
@@ -59,12 +59,12 @@ data[u'Auftraggeber / Begünstigter'].replace('DKB VISACARD', 'Bar', inplace=Tru
 
 # ### Inverse Dataframe to have first day first
 
-# In[225]:
+# In[34]:
 
 data = data.iloc[::-1]
 
 
-# In[226]:
+# In[35]:
 
 fr = data.index[0]
 to = data.index[-1]
@@ -72,14 +72,14 @@ to = data.index[-1]
 
 # ### Cumulative Sum over values plus starting balance
 
-# In[227]:
+# In[36]:
 
 data['Kontostand (EUR)'] = data['Betrag (EUR)'].cumsum()+kst
 
 
 # ### Linear Trend
 
-# In[228]:
+# In[37]:
 
 from scipy.stats import linregress
 def fit_line(x, y):
@@ -88,59 +88,62 @@ def fit_line(x, y):
     return slope, intercept
 
 
-# In[229]:
+# In[38]:
 
 slope, intercept = fit_line( (data.index - data.index[0]).days, data['Kontostand (EUR)'].values)
 
 
-# In[230]:
+# In[39]:
 
 print('You gain %.2fEUR per day.' % slope)
 
 
-# In[231]:
+# In[40]:
 
 data['Kontostand (EUR) linear'] = (data.index - data.index[0]).days * slope + intercept
 
 
-# In[239]:
+# In[41]:
 
 data[['Kontostand (EUR)', 'Kontostand (EUR) linear']].plot(title='Kontostand (DKB #%s)' % knr, figsize=(10,4))
 
 
 # ### Broke or a Million on
 
-# In[264]:
+# In[42]:
 
 from datetime import timedelta
 if slope < 0:
     brokedate = data.index[0] + timedelta(days=((0.0-intercept) / slope))
     print('Pleite am %i.%i.%i' % (brokedate.day, brokedate.month, brokedate.year))
 else:
-    goal = 1000000.0 # million
-    milliondate = data.index[0] + timedelta(days=((goal-intercept) / slope))
-    print('Millionär am %i.%i.%i' % (milliondate.day, milliondate.month, milliondate.year))
+    try:
+        goal = 1000000.0 # million
+        milliondate = data.index[0] + timedelta(days=((goal-intercept) / slope))
+        print(u'Millionär am %i.%i.%i' % (milliondate.day, milliondate.month, milliondate.year))
+    except:
+        print(u'Wahrscheinlich wirst du niemals Millionär!')
 
 
 # ## Who's doin' it?
 
-# In[242]:
+# In[43]:
 
 empfgroup = data.groupby(u'Auftraggeber / Begünstigter')['Betrag (EUR)'].sum()
 
 
-# In[243]:
+# In[44]:
 
 empfgroup.sort()
 
 
-# In[244]:
+# In[45]:
 
 # Short the names
 empfgroup.index = [str(i)[:30] for i in empfgroup.index]
 
 
-# In[247]:
+# In[46]:
 
 empfgroup.plot(kind='barh', figsize=(5,15),
                color=['g' if empfgroup.values[i]>0 else 'r' for i in range(len(empfgroup.values))],
